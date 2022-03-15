915fc6a8b6fc5cb1626aa5073ce9cd2ddade7622
caiquequaresmasilva <caique.quaresma@gmail.com>
2022-03-15 15:28:27 -0300

    Criado Dockerfile do front

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: 'TRYBE_FUTEBOL_CLUBE',
+  database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT || '3002',
   dialect: 'mysql',
