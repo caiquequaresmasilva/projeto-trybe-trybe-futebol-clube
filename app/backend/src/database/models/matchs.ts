4954826077135e2150faa5603a429f3557df82e5
caiquequaresmasilva <caique.quaresma@gmail.com>
2022-03-17 12:34:48 -0300

    Ajustando associaçoes do model Match

--- app/backend/src/database/models/matchs.ts
+++ app/backend/src/database/models/matchs.ts
@@ -30,12 +30,19 @@ Match.init({
     type: DataTypes.BOOLEAN,
     allowNull: false,
   },
+  homeTeam: {
+    type: DataTypes.INTEGER,
+  },
+  awayTeam: {
+    type: DataTypes.INTEGER,
+  },
 
 }, {
   underscored: true,
   sequelize: db,
   modelName: 'Match',
   timestamps: false,
+  tableName: 'matchs',
 });
 
 /**
@@ -45,13 +52,13 @@ Match.init({
 
 // OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
 // OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
-Match.belongsTo(Club, { foreignKey: 'home_team', as: 'homeTeam' });
-Match.belongsTo(Club, { foreignKey: 'away_team', as: 'awayTeam' });
+Match.belongsTo(Club, { foreignKey: 'home_team', as: 'homeClub' });
+Match.belongsTo(Club, { foreignKey: 'away_team', as: 'awayClub' });
 
 // Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
 // Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
 
-Club.hasMany(Match, { foreignKey: 'home_team', as: 'homeMatchs' });
-Club.hasMany(Match, { foreignKey: 'away_team', as: 'awayMatchs' });
+Club.hasMany(Match, { foreignKey: 'home_team', as: 'homeMatches' });
+Club.hasMany(Match, { foreignKey: 'away_team', as: 'awayMatches' });
 
 export default Match;
