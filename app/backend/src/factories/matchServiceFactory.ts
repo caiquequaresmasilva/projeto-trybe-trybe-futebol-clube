6d4177cabeeebd3cceb735d237f22ef3c3b873e1
caiquequaresmasilva <caique.quaresma@gmail.com>
2022-03-25 17:11:10 -0300

    Refatorando matchserviceFactory

--- app/backend/src/factories/matchServiceFactory.ts
+++ app/backend/src/factories/matchServiceFactory.ts
@@ -1,5 +1,11 @@
 import Club from '../database/models/clubs';
 import Match from '../database/models/matchs';
 import { MatchService } from '../services';
+import errorGenFactory from './errorGenFactory';
 
-export default () => new MatchService(Club, Match);
+export default () => {
+  const club = Club;
+  const match = Match;
+  const errorGen = errorGenFactory();
+  return new MatchService(club, match, errorGen);
+};
