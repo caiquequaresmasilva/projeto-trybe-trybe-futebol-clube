123a51a2bcc822dd7bc4bfc92f2c1d7035af11e6
caiquequaresmasilva <caique.quaresma@gmail.com>
2022-03-25 16:38:55 -0300

    Refatorando userService

--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -1,44 +1,31 @@
 import { ILogin } from '../interfaces';
 import User from '../database/models/users';
-import { loginSchema } from '../schemas/user';
-import { Token, checkPassword } from '../utils';
+import { checkPassword, ErrorGen, UserValidations } from '../utils';
 
 export default class UserService {
-  private static tokenManager = Token;
-
-  private static loginSchema = loginSchema;
-
   constructor(
     private userModel: typeof User,
+    private userValidations: UserValidations,
+    private errorGen: ErrorGen,
   ) {}
 
-  static authValidation(token:string | undefined) {
-    if (!token) return { errorCode: 401, message: 'Token not found' };
-    try {
-      const user = UserService.tokenManager.verify(token);
-      return user;
-    } catch (_) {
-      return { errorCode: 401, message: 'Invalid Token' };
-    }
+  authValidation(token:string | undefined) {
+    return this.userValidations.authValidation(token);
   }
 
-  static validateLogin(user: ILogin) {
-    const validation = UserService.loginSchema.validate(user);
-    if (validation.error) {
-      return { errorCode: 401, message: 'All fields must be filled' };
-    }
-    return {};
+  validateLogin(user: ILogin) {
+    return this.userValidations.validateLogin(user);
   }
 
   async login(loginInfo: ILogin) {
     const user = await this.userModel.findOne({ where: { email: loginInfo.email } });
-    if (!user) return { errorCode: 401, message: 'Incorrect email or password' };
+    if (!user) return this.errorGen.unauthorized('Incorrect email or password');
     const { password, username, email, id, role } = user;
     const passFlag = await checkPassword(loginInfo.password, password);
     if (!passFlag) {
-      return { errorCode: 401, message: 'Incorrect email or password' };
+      return this.errorGen.unauthorized('Incorrect email or password');
     }
-    const token = UserService.tokenManager.generate({ username, email, id, role });
+    const token = this.userValidations.tokenManager.generate({ username, email, id, role });
     return { user: { id, username, role, email }, token };
   }
 }
