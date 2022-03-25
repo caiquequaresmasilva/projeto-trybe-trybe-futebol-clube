cc4b0e7dd4a7205d77526ecc3a99c239338f95cc
caiquequaresmasilva <caique.quaresma@gmail.com>
2022-03-25 17:01:26 -0300

    Refatorando matchService

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -1,3 +1,4 @@
+import { ErrorGen } from '../utils';
 import Club from '../database/models/clubs';
 import Match from '../database/models/matchs';
 
@@ -5,6 +6,7 @@ export default class MatchService {
   constructor(
     private clubModel: typeof Club,
     private matchModel: typeof Match,
+    private errorGen: ErrorGen,
   ) {}
 
   async getAll(inProgress: undefined | string) {
@@ -21,8 +23,8 @@ export default class MatchService {
 
   async update(id: string | number, data: Partial<Match>) {
     const match = await this.matchModel.findByPk(id);
-    if (!match) return { errorCode: 404, message: 'Match not found' };
-    if (!match.inProgress) return { errorCode: 401, message: 'Match already finished' };
+    if (!match) return this.errorGen.notFound('Match not found');
+    if (!match.inProgress) return this.errorGen.unauthorized('Match already finished');
     const [updated] = await this.matchModel.update(data, { where: { id } });
     return updated;
   }
@@ -38,13 +40,13 @@ export default class MatchService {
 
   async create(data: Match) {
     if (!MatchService.validateMatch(data)) {
-      return {
-        errorCode: 401, message: 'It is not possible to create a match with two equal teams',
-      };
+      return this.errorGen.unauthorized(
+        'It is not possible to create a match with two equal teams',
+      );
     }
     const teamsFlag = await this.validateTeams(data);
     if (!teamsFlag) {
-      return { errorCode: 401, message: 'There is no team with such id!' };
+      return this.errorGen.unauthorized('There is no team with such id!');
     }
     const { id } = await this.matchModel.create(data);
     return { ...data, id };
