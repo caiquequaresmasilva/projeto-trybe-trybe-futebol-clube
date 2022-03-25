d0f914eae5e0e3a53996b8f1e30b6ebc4cdc11dc
caiquequaresmasilva <caique.quaresma@gmail.com>
2022-03-25 17:12:44 -0300

    Ajustando match Routes

--- app/backend/src/routes/matchesRoutes.ts
+++ app/backend/src/routes/matchesRoutes.ts
@@ -8,8 +8,8 @@ const matchController = matchControllerFactory();
 router.get('/', (req, res) => matchController.getAll(req, res));
 
 router.use(authValidation);
-router.post('/', (req, res) => matchController.create(req, res));
-router.patch('/:id/finish', (req, res) => matchController.finishMatch(req, res));
-router.patch('/:id', (req, res) => matchController.update(req, res));
+router.post('/', (req, res, next) => matchController.create(req, res, next));
+router.patch('/:id/finish', (req, res, next) => matchController.finishMatch(req, res, next));
+router.patch('/:id', (req, res, next) => matchController.update(req, res, next));
 
 export default router;
